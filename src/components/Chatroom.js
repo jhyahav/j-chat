78d9046680861e863ee7c58c2e831a99f55680eb
Jonathan <jhyahav2@gmail.com>
2021-07-20 17:07:04 +0300

    added autoscroll (todo: prevent new received from autoscrolling)

--- src/components/Chatroom.js
+++ src/components/Chatroom.js
@@ -1,14 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import ChatMessage from './ChatMessage';
 
 function Chatroom(props) {
     const messagesRef = props.firestore.collection('messages');
-    const messageQuery = messagesRef.orderBy('createdAt').limit(15);
+    const messageQuery = messagesRef.orderBy('createdAt', 'asc').limitToLast(15);
     const [messages] = useCollectionData(messageQuery, {idField: 'id'});
     const [formValue, setFormValue] = useState('');
+    const scrollRef = useRef();
+    useEffect(() => {scrollRef.current.scrollIntoView({behavior: 'smooth'})}, [messageQuery]);
     const sendMessage = async(event) => {
         event.preventDefault();
         const {uid, photoURL, displayName} = props.auth.currentUser;
@@ -20,13 +22,17 @@ function Chatroom(props) {
             displayName
         })
         setFormValue('');
+        //scrollRef.current.scrollIntoView({behavior: 'smooth'});
     }
+    
 
     return (
         <>
             <div>
                 {messages && messages.map(mes => <ChatMessage key={mes.id} message={mes} auth={props.auth}/>)}
             </div>
+
+            <div ref={scrollRef}></div>
 
             <form onSubmit={sendMessage}>
                 <input value={formValue} onChange={(event) => setFormValue(event.target.value)}/>
