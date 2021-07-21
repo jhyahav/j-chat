befeaa8745544389f3fb1611e58a3ab8c6fcc3c4
Jonathan <jhyahav2@gmail.com>
2021-07-21 23:05:48 +0300

    added support for rtl text (hebrew) todo: move profile pics inline w/ messages

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -3,11 +3,12 @@ function ChatMessage(props) {
     const { uid, text, displayName, photoURL, createdAt } = props.message;
     const messageType = uid === props.auth.currentUser.uid ? 'sent' : 'received';
     const date = createdAt ? createdAt.toDate().toLocaleString() : '';
+    const fullDisplayName = (messageType === 'sent') ? displayName + ' (you)' : displayName;
 
     return (
         <div className={`message ${messageType}`}>
             <img className='profile-pic' src={photoURL} alt={`User profile: ${displayName}`} />
-            <p className='display-name'>{displayName}</p>
+            <p className='display-name'>{fullDisplayName}</p>
             <p className='message-text'>{text}</p>
             <p className='date'>{date}</p>
         </div>
